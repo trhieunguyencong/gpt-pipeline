9cdf54a58bff83c7674f22f7804b4a016e864fbc
Nguyễn Công Trung Hiếu <hieu@example.com>
2025-04-26 22:00:58 +0700

    Fix dữ liệu http >>> data 3

--- run_pipeline_api.py
+++ run_pipeline_api.py
@@ -11,10 +11,15 @@ async def run_pipeline(request: Request):
     data = await request.json()
 
     # Bước 2: Ghi dữ liệu vào file tạm input_data.json
-    with open("input_data.json", "w", encoding="utf-8") as f:
+    import os
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    input_file_path = os.path.join(base_dir, "Timeline_rb", "input_data.json")
+
+    with open(input_file_path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-    print("\n✅ Đã ghi input_data.json thành công!")
+    print(f"\n✅ Đã ghi input_data.json thành công tại {input_file_path}")
 
     # Bước 3: Gọi pipeline rack
     timeline_rack_v5_lazy(
